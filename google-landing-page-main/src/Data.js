45829a20b93e67f5802f6a8c6ec02b03958e042a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-02-02 18:18:30 +0100

    Shortcut-icons

---
+++ google-landing-page-main/src/Data.js
@@ -0,0 +1,42 @@
+const Data = [
+  {
+    icon: "hey",
+    name: "Twitter"
+  },
+  {
+    icon: "hey",
+    name: "Guthub"
+  },
+  {
+    icon: "hey",
+    name: "Behance"
+  },
+  {
+    icon: "hey",
+    name: "Unsplash"
+  },
+  {
+    icon: "hey",
+    name: "Stack Overflow"
+  },
+  {
+    icon: "hey",
+    name: "Netlify"
+  },
+  {
+    icon: "hey",
+    name: "Freepik"
+  },
+  {
+    icon: "hey",
+    name: "Figma"
+  },
+  {
+    icon: "hey",
+    name: "Medium"
+  },
+  {
+    icon: "hey",
+    name: "Instagram"
+  }
+]
