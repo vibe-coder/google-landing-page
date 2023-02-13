ba5c216343fc9dfd7d04d35e0cef8e1b6ed785b4
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-02-13 12:31:35 +0100

    Customize wrapper

--- google-landing-page-main/src/BottomSection.js
+++ google-landing-page-main/src/BottomSection.js
@@ -4,9 +4,16 @@ import PencilIcon from "./pencil.png"
 export default function BottomSection(){
   return(
     <div className="bottom-section">
-      <div className="pencil-wrapper">
-        <img className="pencil-icon" src={PencilIcon} alt="Pencil Icon"/>
+      <div className="customize-wrapper">
+        <ul className="list-items">
+          <p>Color</p>
+          <p>Background</p>
+          <p>Color</p>
+        </ul>
       </div>
+      <button className="pencil-button">
+        <img className="pencil-icon" src={PencilIcon} alt="Pencil Icon"/>
+      </button>
       
     </div>
   )
