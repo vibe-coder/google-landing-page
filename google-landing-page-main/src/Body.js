383972ff9eff38d970d0ec8c40a10fb7301b0f15
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-02-14 09:52:43 +0100

    Collapsible achieved

--- google-landing-page-main/src/Body.js
+++ google-landing-page-main/src/Body.js
@@ -5,7 +5,7 @@ function Body() {
   return (
     <div className="body-wrapper">
       <header className="body">
-        <img className="bg-image" src="./pics/pic-1.jpg"  alt="backgroundImage"/>
+        <img className="bg-image" src="./pics/pic-4.jpg"  alt="backgroundImage"/>
       </header>
     </div>
   );
--- google-landing-page-main/src/BottomSection.js
+++ google-landing-page-main/src/BottomSection.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React,  { useState } from "react";
+import "./App.css"
 import PencilIcon from "./pencil.png"
 
 export default function BottomSection(){
+
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
   return(
     <div className="bottom-section">
-      <div className="customize-wrapper">
-        <ul className="list-items">
-          <p>Color</p>
-          <p>Background</p>
-          <p>Color</p>
-        </ul>
-      </div>
-      <button className="pencil-button">
+      {isOpen && <div className="customize-wrapper">
+        <p>Background</p>
+        <p>Shortcuts</p>
+        <p>Color and Theme</p>
+      </div>}
+      
+      <button onClick={() => setIsOpen(!isOpen)} className="pencil-button">
         <img className="pencil-icon" src={PencilIcon} alt="Pencil Icon"/>
       </button>
       
